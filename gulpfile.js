5bd665959f243bbc7f50cd7cd0e2c2b9385fb0ef
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2023-08-10 22:21:21 -0600

    Implementar plumber para no detener la ejecución de la compilación de sass si esta contiene errores

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
-  src("src/scss/app.scss") //-Identificar archivo de SASS
-    .pipe(sass()) //-Compilarlo
-    .pipe(dest("build/css")); //-Almacenarlo en el disco duro
+  src("src/scss/app.scss") //*Identificar archivo de SASS
+    .pipe(plumber()) //* Sirve para no detener el WorkFlow cuando un archivo compile mal
+    .pipe(sass()) //*Compilarlo
+    .pipe(dest("build/css")); //*Almacenarlo en el disco duro
   done(); //¡CAllBACK que avisa a gulp cuando llegamos al final
 }
 
