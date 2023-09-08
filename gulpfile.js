fde17e1b3e97970c88a18ab6281d500807b26d69
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2023-09-08 00:09:41 -0600

    Conversión de imagenes y creacion de funcion

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+//- CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+//- IMAGENES
+const webp = require("gulp-webp");
 
 function css(done) {
   src("src/scss/app.scss") //*Identificar archivo de SASS
@@ -10,10 +14,20 @@ function css(done) {
   done(); //¡CAllBACK que avisa a gulp cuando llegamos al final
 }
 
+//| Convertir Imagenes
+function versionWebp(done) {
+  const opciones = { quality: 50 };
+  src("src/img/**/*.{png,jpg}") //* De esta manera escojo todos loa archivos de ciertas extenciones de manera recursiva
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.convertirWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
